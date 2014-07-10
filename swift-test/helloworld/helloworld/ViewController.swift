baff9aa2f9883f613ffa48ef22bcd2187c914e95
chenjingyu <chenjingyu@MacBook-Pro.local>
2014-07-10 17:16:02 +0800

    7.10

---
+++ swift-test/helloworld/helloworld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by chenjingyu on 14/7/10.
+//  Copyright (c) 2014年 chenjingyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet var labelHelloworld: UITextField
+
+}
+
